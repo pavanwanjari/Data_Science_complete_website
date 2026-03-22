aa2dbea1ce09871ab78dd0053db47aa2e2997ef2
Pavan Wanjari <92113558+pavanwanjari@users.noreply.github.com>
2026-03-23 00:35:10 +0530

    Fix free combo checkout and cart styling

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DL10X_CONFIG = {
-  SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbynjfLEISwk9m3Zoe4bX4vR5u2rwsEcHhfzUk4LsqvC6th4rYm0d-bXlSroMCz7EVZX/exec",
+  SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbypIyL48VhqzRy_osCCO-RXFhty7I4hwW2w0C6u4mcuYy8Onq9Yb_XxMGGjWUKAvb4/exec",
   WHATSAPP_NUMBER: "919403030512",
   WHATSAPP_TEXT: "Hi DataLearn10X, I want details about courses",
   AD_IMAGES_BASE_URL: "ads",
